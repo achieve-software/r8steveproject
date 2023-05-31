c2de12bb803eaf63f1b4957099a8966577a2860e
Achieve-software <vtcbcdtyr@gmail.com>
2023-05-31 14:52:52 +0300

    st

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,16 @@
-import Nav from "../../nav/Nav";
-import "./Header.css"
+import Nav from "../nav/Nav";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="headerRight">
-      <h3>
-          <span className="alex">achieve-software </span>(Full Stack Developer)
+        <h3>
+          <span className="alex">achieve-software </span>( Full-Stack Developer)
         </h3>
       </div>
-      <Nav/>
+
+      <Nav />
     </div>
   );
 };

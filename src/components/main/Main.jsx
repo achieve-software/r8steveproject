d8f4a776c548a6976b7af0ef4b9c348145e63a1f
Achieve-software <vtcbcdtyr@gmail.com>
2024-06-21 03:16:31 +0300

    gg

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,7 +7,7 @@ import "./Main.css";const Main = (props) => {
         <div className="desc">
           <p>{desc}</p>
         </div>
-        <button className="btn"> Detail</button>
+        <button className="btn">More Detail</button>
       </div>
     </div>
   );
